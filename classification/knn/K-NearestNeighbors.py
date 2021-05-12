815d0e2e4174e5f1f23ba2dfcd8c27650b5c12ee
Andrey Shimanskiy <andrey.shimanskiy@rt.ru>
2021-05-12 14:23:07 +0300

    Рефакторинг knn

--- classification/knn/K-NearestNeighbors.py
+++ classification/knn/K-NearestNeighbors.py
@@ -1,41 +1,42 @@
 from types import FunctionType
-import numpy as np
-from numpy.lib.function_base import copy
+
 from pandas.core.frame import DataFrame
-from sklearn import neighbors,datasets
+from sklearn import neighbors, datasets
 from sklearn.model_selection import train_test_split
 
 
 class KnnResult:
-    def __init__(self,predictionResult,scopeResult,testData,classifier) -> None:
-        self.predictionResult=predictionResult
-        self.scope=scopeResult
-        self.knnClassifier=classifier
-        self.testData=testData
+    def __init__(self, prediction_result, scope_result, test_data, classifier) -> None:
+        self.predictionResult = prediction_result
+        self.scope = scope_result
+        self.knnClassifier = classifier
+        self.testData = test_data
         pass
 
-def knn(n_neighbors:int,weights:FunctionType,data:DataFrame,targetColumn='target',testSize=0.2)->KnnResult:
-    
-    X=data.drop(columns=[targetColumn])
-    Y=data[targetColumn].values
-    X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=testSize,random_state=1,stratify=Y)
 
-    classifier=neighbors.KNeighborsClassifier(n_neighbors=n_neighbors,weights=weights)
-    classifier.fit(X=X_train,y=Y_train)
-    return KnnResult(predictionResult=classifier.predict(X=X_test),
-        scopeResult=classifier.score(X=X_test,y=Y_test),
-        classifier=classifier,testData=X_test)
+def knn(n_neighbors: int, weights: FunctionType, data: DataFrame, target_column='target', test_size=0.2) -> KnnResult:
+    X = data.drop(columns=[target_column])
+    Y = data[target_column].values
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=test_size, random_state=1, stratify=Y)
+
+    classifier = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, weights=weights)
+    classifier.fit(X=X_train, y=Y_train)
+    return KnnResult(prediction_result=classifier.predict(X=X_test),
+                     scope_result=classifier.score(X=X_test, y=Y_test),
+                     classifier=classifier, test_data=X_test)
+
 
 def main():
-    iris=datasets.load_iris()
-    iris_frame=DataFrame(iris.data)
-    iris_frame.columns=iris.feature_names
-    iris_frame['target']=iris.target
-    iris_frame['name']=iris_frame.target.apply(lambda x:iris.target_names[x])
-    iris_frame=iris_frame.drop(columns=["name"])
-    result=knn(10,'uniform',iris_frame,'target',0.25)
+    iris = datasets.load_iris()
+    iris_frame = DataFrame(iris.data)
+    iris_frame.columns = iris.feature_names
+    iris_frame['target'] = iris.target
+    iris_frame['name'] = iris_frame.target.apply(lambda x: iris.target_names[x])
+    iris_frame = iris_frame.drop(columns=["name"])
+    result = knn(10, 'uniform', iris_frame, 'target', 0.25)
 
     print(result.scope)
     print(result.predictionResult)
+
 
 main()
