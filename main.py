859e174fe2b36d42e78493c531d3a8c3363e52c0
Aleksey Gorovoy <aleksey.gorovoy@rt.ru>
2021-05-19 00:33:19 +0300

    Added initial ui

--- main.py
+++ main.py
@@ -2,15 +2,57 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from tkinter import *
+from tkinter import scrolledtext, filedialog
+
+from os import getcwd
+
+def main():
+    window = Tk()
+    window.title("Text analyzer")
+    window.geometry('700x300')
+
+    label = Label(window, text="Файлы")
+    label.place(x=10, y=10)
+
+    listbox = Listbox(window, width=40, height=10)
+    listbox.place(x=10, y=40)
+
+    add_file_button = Button(window, text="Добавить файл", command=lambda: add_file_button_clicked(listbox))
+    add_file_button.place(x=270, y=40)
+
+    delete_file_button = Button(window, text="Удалить файл", command=lambda: delete_file_button_clicked(listbox))
+    delete_file_button.place(x=270, y=70)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    alg = IntVar()
+
+    clust_x = 450
+    Label(window, text="Кластеризация").place(x=clust_x, y=10)
+
+    Radiobutton(window, text="ДБ сканирование", variable=alg, value=0).place(x=clust_x, y=40)
+    Radiobutton(window, text="K means", variable=alg, value=1).place(x=clust_x, y=70)
 
 
+    class_x = 570
+    Label(window, text="Классификация").place(x=class_x, y=10)
+
+    Radiobutton(window, text="K ближайшие", variable=alg, value=2).place(x=class_x, y=40)
+    Radiobutton(window, text="Наивный Байес", variable=alg, value=3).place(x=class_x, y=70)
+
+
+    window.mainloop()
+
+def add_file_button_clicked(listbox: Listbox):
+    filename = filedialog.askopenfilename(initialdir=getcwd())
+    listbox.insert(END, filename)
+
+def delete_file_button_clicked(listbox: Listbox):
+    index = listbox.curselection()
+    if index != ():
+        listbox.delete(index)
+    
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
